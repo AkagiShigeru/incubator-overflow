f43f7e5f7bd96893d2c45fa7f02ac2208e5c6fb4
Alexander Schulz <alexsminfeld@gmx.de>
2017-10-17 16:44:26 +0200

    further files preparing for coming analysis.

---
+++ analyse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  Tools to analyse and plot some data
+#
+#
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
