710bfb347967c345f4ddc5b93474670c43124858
Alexander Schulz <alexsminfeld@gmx.de>
2017-10-17 16:43:53 +0200

    Restructure, working version of read-in into hdf.

---
+++ stack_readin.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  Tools to read and convert stackoverflow xml files into hdf files
+#
+#
+import os
+from libarchive.public import file_reader
+import gzip
+
+import pandas as pd
+
+from collections import defaultdict
+
+from IPython import embed
+
+from stack_util import *
+
+
+cols_and_defaults = {"AcceptedAnswerId": -1, "AnswerCount": -1, "Body": "", "ClosedDate": "",
+                     "CommentCount": -1, "CommunityOwnedDate": "", "CreationDate": "",
+                     "FavoriteCount": -1, "Id": -1, "LastActivityDate": "", "LastEditDate": "",
+                     "LastEditorDisplayName": "", "LastEditorUserId": -1, "OwnerDisplayName": "",
+                     "OwnerUserId": -1, "ParentId": -1, "PostTypeId": -1, "Score": -1,
+                     "Tags": "", "Title": "", "ViewCount": -1}
+
+cols_and_dtypes = {"AcceptedAnswerId": int, "AnswerCount": int, "Body": str, "ClosedDate": str,
+                   "CommentCount": int, "CommunityOwnedDate": str, "CreationDate": str,
+                   "FavoriteCount": int, "Id": int, "LastActivityDate": str, "LastEditDate": str,
+                   "LastEditorDisplayName": str, "LastEditorUserId": int, "OwnerDisplayName": str,
+                   "OwnerUserId": int, "ParentId": int, "PostTypeId": int, "Score": int,
+                   "Tags": str, "Title": str, "ViewCount": int}
+
+# user-defined columns with derived quantities
+own_cols = {"BodySize": lambda e: len(e["Body"]),
+            "BodyNQMarks": lambda e: e["Body"].count("?")}
+
+cols_and_converters = {"Tags": lambda t: CleanTags(t)[:150], "Body": lambda x: "",
+                       "Title": lambda t: t[:300]}
+
+
+def IterateZippedXML(zf, delim=" />\r\n  <row", debug=False):
+    """ Generator that returns lines of zipped file line by line."""
+    with file_reader(zf) as zhandler:
+        for onefile in zhandler:
+            line = ""
+            for block in onefile.get_blocks():
+                line += block
+                if debug:
+                    print block
+                while delim in line:
+                    pos = line.index(delim)
+                    yield line[:pos].strip() + "/>"
+                    line = line[pos + len(delim):].strip()
+                if delim in line:
+                    yield line + "/>"
+
+
+# TODO: function to read split and unpacked XML in parallel (using pmap)
+def ReadInParallel(xmls):
+    pass
+
+
+def CreateHDFStores(finp, outstore, dump_posts=False, limit=None):
+    """ Uses other utility function to create hdf store with DataFrame of posts.
+        Optionally dumps individual posts into zipped files on the hard disk."""
+    base = os.path.split(finp)[0]
+
+    post_dict = defaultdict(list)
+
+    store = pd.HDFStore(outstore, "w", complib="blosc", complevel=9)
+    store.put("posts", pd.DataFrame(), format="table", data_columns=True)
+
+    n = 0
+    for post in IterateZippedXML(finp):
+
+        n += 1
+
+        # read-in limit reached
+        if limit and n > limit:
+            break
+
+        entrydict = dict(ParsePostFromXML(post))
+
+        # dump actual post entries into seperate files (not to blow up dataframe too much)
+        if dump_posts:
+            with gzip.open(os.path.join(base, "post_%i.txt.gz" % entrydict["Id"], "wb")) as f:
+                f.write(UnescapeHTML(entrydict["Body"]))
+
+        for ename, edefault in cols_and_defaults.items():
+
+            if ename in entrydict:
+                val = entrydict[ename]
+
+                # optional conversion function
+                if ename in cols_and_converters:
+                    val = cols_and_converters[ename](val)
+
+                # dtype conversion
+                dtype = cols_and_dtypes[ename]
+                if not isinstance(val, dtype):
+                    try:
+                        val = dtype(val)
+                    except:
+                        AssertionError("Couldn't convert %s to proper dtype!" % val)
+
+                post_dict[ename].append(val)
+            else:
+                post_dict[ename].append(edefault)
+
+        for entry in entrydict:
+            assert entry in cols_and_defaults, "Unexpected entry with key: %s" % entry
+
+        # calculate and append custom entries
+        for custom in own_cols:
+            post_dict[custom].append(own_cols[custom](entrydict))
+
+        if n % 1000000 == 0:
+
+            print "Processed %i posts!" % n
+
+            df = pd.DataFrame(post_dict)
+            store.append("posts", df, format="table", data_columns=True,
+                         min_itemsize={"Title": 300, "Tags": 150})
+            post_dict.clear()
+            del df
+
+    # we need to push the remainder of posts left in the dictionary
+    if len(post_dict.values()[0]):
+
+        df = pd.DataFrame(post_dict)
+        store.append("posts", df, format="table", data_columns=True)
+        post_dict.clear()
+        del df
+
+    store.close()
+
+    return True
+
+
+if __name__ == "__main__":
+    f = "/home/alex/data/stackexchange/overflow/stackoverflow.com-Posts.7z"
+
+    CreateHDFStores(f, "/home/alex/data/stackexchange/overflow/caches/posts.hdf5")
+
+    # for testing
+    # i = 0
+    # cols = defaultdict(int)
+    # for l in IterateZippedXML(f):
+    #     i += 1
+    #     if i < 3:
+    #         continue
+    #     res = dict(ParsePostFromXML(l))
+    #     if "Tags" in res:
+    #         print res
+    #         embed()
+    #     for k in res.keys():
+    #         cols[k] += 1
+    #     if i == 1000000:
+    #         break
--- stack_util.py
+++ stack_util.py
@@ -1,72 +1,39 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-#  Tools to read and convert stackoverflow xml files
+#  Simple utility functions used within the other scripts
 #
-from libarchive.public import file_reader
-
-from collections import defaultdict
-import pandas as pd
+#
 import re
 
 from lxml.html import fromstring
-# from xml.etree import cElementTree
-from IPython import embed
+from HTMLParser import HTMLParser
 
 
-def IterateZippedXML(zf, delim="\n", debug=False):
-    """ Generator that returns lines of zipped file line by line."""
-    with file_reader(zf) as zhandler:
-        for onefile in zhandler:
-            line = ""
-            for block in onefile.get_blocks():
-                line += block
-                if debug:
-                    print block
-                while delim in line:
-                    pos = line.index(delim) + 1
-                    yield line[:pos].strip()
-                    line = line[pos:].strip()
-                yield line
+hparser = HTMLParser()
 
 
 def CleanString(s):
     return fromstring(s).text_content().encode("ascii", "ignore")
 
 
-def InitializePostsParser():
-    # format: <row Id="number" PostTypeId="number" CreationDate="2008-08-01T00:42:38.903" Score="number"
-    #          ViewCount="number" Body="any text" OwnerUserId="number" LastEditorUserId="number"
-    #          LastEditorDisplayName="name" LastEditDate="2016-12-20T19:22:26.510"
-    #          LastActivityDate="2017-07-31T11:30:41.573"
-    #          Title="title text" Tags="tag1;tag2;tag3"
-    #          AnswerCount="number" CommentCount="number" FavoriteCount="number" />
-    parser = re.compile(r"\s?([A-Z][A-Za-z]+)=\"(.*)\"\s?")
-    return parser
+def UnescapeHTML(s):
+    return hparser.unescape(s)
 
 
-def ParsePost(l, parser):
-    # return [m.groupdict() for m in parser.finditer(l)]
+def ParsePostFromXML(l):
     return re.findall(r"\s?([A-Z][A-Za-z]+)=\"([^\"]*)\"\s?", l)
 
-# todo
-# write out stuff in data frame
 
-if __name__ == "__main__":
-    f = "/home/alex/data/stackexchange/overflow/stackoverflow.com-Posts.7z"
+def CleanTags(tagstring):
+    return tagstring.replace("&gt;", "").replace("&lt;", ";")
 
-    parser = InitializePostsParser()
 
-    i = 0
-    for l in IterateZippedXML(f):
-        if i < 1:
-            i += 1
-            continue
-        print l
-        print CleanString(l)
-        print ParsePost(l, parser)
-        i += 1
-        if i == 3:
-            break
-
-    # embed()
+def SplitTags(tagstring):
+    """ Splits according to html entities of <> which bundle tags in the xml files."""
+    cleaned = tagstring.replace("&gt;", "")
+    if cleaned != "":
+        split = cleaned.split("&lt;")
+        if len(split) > 0:
+            return split[1:]
+    return []
