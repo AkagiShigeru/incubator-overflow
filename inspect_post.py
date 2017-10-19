a82df1d2df9207d559f447f646d12e6f610dbaf7
Alexander Schulz <alexsminfeld@gmx.de>
2017-10-19 16:29:55 +0200

    print also useful meta info for a given post (will be useful for some checks and studies later).

--- inspect_post.py
+++ inspect_post.py
@@ -5,16 +5,30 @@
 #
 #
 import os
+import re
 from glob import glob
 import numpy as np
+import pandas as pd
 import gzip
 
 files = glob("/home/alex/data/stackexchange/overflow/posts/*.txt.gz")
+hdfpath = "/home/alex/data/stackexchange/overflow/caches/posts_all.hdf5"
 
 print "Number of posts in directory:", len(files)
 
-# rani = 234344
+# rani = 1798777
 rani = np.random.randint(len(files))
 
 with gzip.open(files[rani]) as gf:
+    print ">>>"
     print gf.read()
+    print "<<<"
+
+store = pd.HDFStore(hdfpath, "r", complib="blosc", complevel=9)
+
+cols = None
+smask = store.select_as_coordinates("posts", "Id == %i" % int(re.findall(r"\d+", os.path.split(files[rani])[-1])[0]))
+posts = store.select("posts", where=smask)
+
+print "Information about this post:\n"
+print posts.iloc[0]
