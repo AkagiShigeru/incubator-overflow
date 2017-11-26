539c3e0523b6081fc018bca4542ca988f903243a
Alexander Schulz <alexsminfeld@gmx.de>
2017-11-26 16:31:20 +0100

    moving function to better location.

--- stack_util.py
+++ stack_util.py
@@ -36,6 +36,67 @@ cols_desc = {"AnswerCount": "Number of replies", "BodyNCodes": "Number of code t
 hparser = HTMLParser()
 
 
+# The function "text_to_wordlist" is from
+# https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
+# adapted slightly
+def TextCleansing(text, remove_stopwords=True, stem_words=False):
+    """ Clean the text, with the option to remove stopwords and to stem words. """
+    try:
+        from spacy.lang.en import STOP_WORDS as STOPWORDS
+    except:
+        from spacy.en import STOPWORDS
+
+    from nltk.stem import SnowballStemmer
+
+    text = text.lower()
+
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+
+    # Optionally, remove stop words
+    if remove_stopwords:
+        text = text.split()
+        text = [w for w in text if w not in STOPWORDS]
+        text = " ".join(text)
+
+    # Optionally, shorten words to their stems
+    if stem_words:
+        text = text.split()
+        stemmer = SnowballStemmer("english")
+        stemmed_words = [stemmer.stem(word) for word in text]
+        text = " ".join(stemmed_words)
+
+    return text
+
+
 def YieldDBPosts(conn, nchunk=1000):
     cursor = conn.execute("SELECT post FROM posts")
     while True:
